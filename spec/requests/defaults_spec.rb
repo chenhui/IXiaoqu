ca0033ab80cb2da8fe44c149e7c93a7030f17e1c
chenhui <xiaotian712572@163.com>
2015-02-08 15:47:04 -0500

    change default web

--- spec/requests/defaults_spec.rb
+++ spec/requests/defaults_spec.rb
@@ -8,26 +8,26 @@ RSpec.describe "Defaults", :type => :request do
 
   describe "Home page" do
 		before{ visit home_path }	    	 
-   	it { should  have_content('home') }
+   	it { should  have_selector('h1',text:'首页') }
     it { should  have_title("#{base_title}|首页")}
   end
 
   describe "Help page" do
   	before { visit help_path }
-  	it { should have_content('help')}
+  	it { should have_selector('h1',text:'帮助')}
     it { should  have_title("#{base_title}|帮助")}
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('about us')}
+    it { should have_selector('h1', text: '关于我们')}
     it { should  have_title("#{base_title}|关于我们")}
   end
 
 
   describe "Contact us page" do
     before { visit contact_path }
-    it { should have_content('contact us')}
+    it { should have_selector('h1',text:'联系我们')}
     it { should  have_title("#{base_title}|联系我们")}
   end
 
