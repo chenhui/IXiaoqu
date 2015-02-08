6b84f3f37e86dc6af64a7407c33a5ab4cba778eb
chenhui <xiaotian712572@163.com>
2015-02-08 15:11:38 -0500

    It is start page of my neighborhood

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+
+	def full_title(page_title)
+		base_title="滨文苑"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title}|#{page_title}"
+		end
+	end
+	
+end
---
+++ spec/requests/defaults_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Defaults", :type => :request do
+
+  subject { page }
+  
+  let(:base_title){ "滨文苑" }
+
+  describe "Home page" do
+		before{ visit home_path }	    	 
+   	it { should  have_content('home') }
+    it { should  have_title("#{base_title}|首页")}
+  end
+
+  describe "Help page" do
+  	before { visit help_path }
+  	it { should have_content('help')}
+    it { should  have_title("#{base_title}|帮助")}
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    it { should have_content('about us')}
+    it { should  have_title("#{base_title}|关于我们")}
+  end
+
+
+  describe "Contact us page" do
+    before { visit contact_path }
+    it { should have_content('contact us')}
+    it { should  have_title("#{base_title}|联系我们")}
+  end
+
+end
