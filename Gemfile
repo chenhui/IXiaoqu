bae8ec4ca2b12ec5cd4a020dc077d49ec68aa7ae
chenhui <xiaotian712572@163.com>
2015-05-30 16:43:04 -0400

    finish flash in sign up

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'sqlite3',group: :development
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass'
+gem 'bootstrap_form'
 gem 'bcrypt'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,5 +11,30 @@ RSpec.describe "UserPages", :type => :request do
 		it { should have_title(user.name)}	
 	end
 
+	describe "signup" do
+
+		before { visit signup_path }	
+		let(:submit){"提交"}
+
+		describe "with invalid information"	 do
+			it "should not create a user"	do
+				expect {click_button submit }.not_to change(User,:count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "姓名"	,with:"chenhui"
+				fill_in "邮件",with:"chenhui@sohu.com"
+				fill_in "密码",with:"foobar"
+				fill_in "确认密码",with:"foobar"
+			end
+
+			it "should create a user" do
+				expect{ click_button submit }.to change(User,:count).by(1)
+			end
+			
+		end
+	end
 
 end
